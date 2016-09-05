d84ef00837ed8a9a66d6898d7cfbf1c07cd9dece
Leo Burdianov <leo.burdianov@gmail.com>
2016-09-05 22:20:43 +0400

    Create new Fragments for each tab

--- app/src/main/java/com/testography/instagramm/activities/MainActivity.java
+++ app/src/main/java/com/testography/instagramm/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testography.instagramm;
+package com.testography.instagramm.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.testography.instagramm.R;
 
 public class MainActivity extends AppCompatActivity {
 
